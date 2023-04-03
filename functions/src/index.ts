e6228bf5b78ec7ce45016b7a4a6fb36f5bd87dd1
So-Won-Kim <kim3007@purdue.edu>
2023-04-02 21:55:43 -0400

    quick update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,13 @@
-"use strict";
+
 const upload = require("./upload");
-//app.use(express.json());
-//app.use(cors());
+// app.use(express.json());
+// app.use(cors());
+
 const auth = require("./auth");
 // auth function
 exports.auth = auth.auth;
 exports.upload = upload.upload;
-/*app.listen(3005, () => {
+
+/* app.listen(3005, () => {
   console.log(`Server is listening at port ${3005}`);
-})*/ 
-//# sourceMappingURL=index.js.map
+})*/
--- functions/src/upload.ts
+++ functions/src/upload.ts
@@ -1,15 +1,15 @@
-//const functions = require("firebase-admin");
+// const functions = require("firebase-admin");
 const functions = require("firebase-functions");
 const cors = require("cors")({origin: true});
 const admin = require("firebase-admin");
-var fs = require('fs');
+const fs = require("fs");
 /**
  * Upload the file in firestore storage
- * @param {String} filePath 
- * @param {String} fileName 
+ * @param {String} filePath
+ * @param {String} fileName
  */
 
-//async function uploadLocalFileToStorage(filePath: string, fileName: string) {
+// async function uploadLocalFileToStorage(filePath: string, fileName: string) {
 exports.upload= functions.https.onRequest((req: any, res: any) => {
   const handleError = (filepath: string, error: any) => {
     functions.logger.error({User: filepath}, error);
@@ -34,30 +34,30 @@ exports.upload= functions.https.onRequest((req: any, res: any) => {
   };
 
   const imageBucket = "images/";
-  
+
   const bucket = admin.storage().bucket();
   const destination = `${imageBucket}${req.body.fileName}`;
 
   console.log(req.body.fileName);
   console.log(req.body.filePath);
-  var stats = fs.statSync(req.body.filePath);
-  console.log('is directory ? ' + stats.isFile());
+  const stats = fs.statSync(req.body.filePath);
+  console.log("is directory ? " + stats.isFile());
 
   try {
     return cors(req, res, async () => {
       // Uploads a local file to the bucket
       await bucket.upload(req.body.filePath, {
-          destination: destination,
-          gzip: true,
-          metadata: {
-              cacheControl: 'public, max-age=31536000',
-          },
+        destination: destination,
+        gzip: true,
+        metadata: {
+          cacheControl: "public, max-age=31536000",
+        },
       });
 
       console.log(`${req.body.fileName} uploaded to /${imageBucket}/${req.body.fileName}.`);
       return handleResponse(req.body.fileName, 200, `${req.body.fileName} uploaded to /${imageBucket}/${req.body.fileName}.`);
     });
   } catch (e) {
-      return handleError(req.body.filePath, e);
+    return handleError(req.body.filePath, e);
   }
 });
